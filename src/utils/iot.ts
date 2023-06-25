93b8f53d3361df6ed987d9384bca6567ed9cbc63
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-06-25 19:30:47 -0400

    fix import env variables

--- src/utils/iot.ts
+++ src/utils/iot.ts
@@ -9,15 +9,13 @@ import {
 
 import { shadow } from './types'
 
-import dotenv from 'dotenv'
-dotenv.config()
 
-if (!process.env.AWS_ACCESS_KEY_ID)
+if (!(import.meta.env.VITE_AWS_ACCESS_KEY_ID))
   throw new Error('AWS_ACCESS_KEY_ID is not defined')
-if (!process.env.AWS_SECRET_ACCESS_KEY)
+if (!(import.meta.env.VITE_AWS_SECRET_ACCESS_KEY))
   throw new Error('AWS_SECRET_ACCESS_KEY is not defined')
-if (!process.env.AWS_REGION) throw new Error('AWS_REGION is not defined')
-if (!process.env.AWS_ENDPOINT) throw new Error('AWS_ENDPOINT is not defined')
+if (!(import.meta.env.VITE_AWS_REGION)) throw new Error('AWS_REGION is not defined')
+if (!(import.meta.env.VITE_AWS_ENDPOINT)) throw new Error('AWS_ENDPOINT is not defined')
 
 const client = new IoTDataPlaneClient({ endpoint: process.env.AWS_ENDPOINT })
 
